fcd68066d3fbe77733239a86035ca30e8fb43851
Muh Habib Ahnaf A <muhhabib.ahnaf@gmail.com>
2022-11-24 10:56:06 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Hello world! <br>";
+
+$nama = "Habib";
+$usia = 22;
+
+echo "Nama saya adalah <strong>$nama</strong>, saya berusia $usia <br>";
+
+$namaKamu = "Ahnaf";
+$usiaKamu = 13;
+
+echo "Nama kamu adalah <strong>$namaKamu</strong>, kamu berusia $usiaKamu <br>";
+
+$selisihUmur = $usia - $usiaKamu;
+
+echo "Selisih umur saya dan kamu adalah $selisihUmur"
+
+?>
